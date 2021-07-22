2fb20eee40d081f04c018bae697664c1ec5ecb00
Junaid <sj.shaikhjunaid@gmail.com>
2021-07-22 17:36:19 +0530

    dynamically add menu items

--- js/index.js
+++ js/index.js
@@ -71,4 +71,41 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const menuItem = document.querySelector(".menu-item");
+
+function createMenuItems() {
+  const items = menu.map((item) => {
+    return `<article>
+      <div class="menu-item-image">
+        <img src="${item.img}" alt="menu-item" />
+      </div>
+      <div class="menu-item-info">
+        <header>
+          <h3 class="menu-item-title">${item.title}</h3>
+          <p class="menu-item-price">$${item.price}</p>
+        </header>
+        <div class="menu-item-desc">
+          <p>
+            ${item.desc}
+          </p>
+        </div>
+      </div>
+    </article>`;
+  });
+
+  return items;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  menuItem.innerHTML = createMenuItems().join(" ");
+});
